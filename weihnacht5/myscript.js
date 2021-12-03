88adb2c09d56f030388db5a519effaaf6c90c499
fambueschleb <fambueschleb@t-online.de>
2021-12-03 21:02:30 +0100

    myscript fotos

--- weihnacht5/myscript.js
+++ weihnacht5/myscript.js
@@ -1,6 +1,6 @@
-var v = new Array();
 var prev = "https://drive.google.com/uc?export=download&id=";
 
+var v = new Array();
 v[1] = [prev + "1lmdYNFWnXXa91doyhv2ONXnW2ThRKsbA3A"];
 v[2] = [prev + "1pcsaKoA0xRGGTj8yD0Pa5-4_c9AqDg7uvw"];
 v[3] = [prev + "1u2lp6UbZ6VWvTaH8L48SflQQlst0dD98RQ"];
@@ -25,41 +25,55 @@ v[20] = [prev + "1GBd3SF1VNO8fsmaiOxUAlPJaW9lsySGQ"];
 v[21] = [prev + "1aQEdDzS_VQgv7c30F4y8csUWbh2Rhk9K"];
 v[22] = [prev + "1J98u3Waig-rFGRd8wrd8hZch_NBh_9lr"];
 v[23] = [prev + "1W8zOD1BuXU_14NHTiMPbtg8cAv2ejrzw"];
-v[24] = [prev + "1C_MqfsaDaKAjK7CzU_KXdKzMd0PoWdLs"];
-v[25] = [prev + "1no8uMhncCnF6C1dRFiQjDAYECO96c4JA"];
-v[26] = [prev + "1l9a3PQBmMSLHGaQulMmRUESqSEI1Z2wN"];
-v[27] = [prev + "1G8WuJXlJsddNVSm55DsdMFEXJDiUA_zu"];
-v[28] = [prev + "16cYhvMDcnUifQev9RMMNO6lxwpVWoZIr"];
+v[24] = [""];
+
+var p = new Array();
+p[1] = [prev + "1C_MqfsaDaKAjK7CzU_KXdKzMd0PoWdLs"];
+p[2] = [prev + "1no8uMhncCnF6C1dRFiQjDAYECO96c4JA"];
+p[3] = [prev + "1l9a3PQBmMSLHGaQulMmRUESqSEI1Z2wN"];
+p[4] = [prev + "1G8WuJXlJsddNVSm55DsdMFEXJDiUA_zu"];
+p[5] = [prev + "16cYhvMDcnUifQev9RMMNO6lxwpVWoZIr"];
+p[6] = [prev + "1QyXMiJVn4NRxxqAg7t9BIQ2n7R7aB6M5"];
+p[7] = [prev + "1TYvnB3mUfgEDwvPeIm7CtSbg5cRSyj2c"];
+p[8] = [prev + "1-sgc0WIGmGm59tzNUKIjnDM_NWjhU5tm"];
+p[9] = [prev + "1S3wCq9ErQpztHELk0ip6iVPmu3Zlkju4"];
+p[10] = [prev + "1kXSYWRC6B0BvlSgASBa7lrlX-XwaRPI2"];
+p[11] = [prev + "1_L2a0G-TUvLfkHid8QDqWdH2O0G-nEWo"];
+p[12] = [prev + "1Xgl6d1PaEStSMNUSMVNPvgH2EnVdmQm3"];
+p[13] = [prev + "1VWujhnXDckNWxpWWoRVW7FG21Qza7yLg"];
+p[14] = [prev + "193u4Aa9OMEahy64zlpy0gQW2WkKFMcob"];
+p[15] = [prev + "1mMtcv0tkHTUWawxSR4IDQNEjbfhPqPs3"];
+p[16] = [prev + "1dsTBr0uj-22-mewqrCOYNFahacrtoKlo"];
+p[17] = [prev + "1B6CckKOtU5kD-7RoNxC50tyMsA9cZ1Pg"];
+p[18] = [prev + "1nMI5x4CZIGOJE1Sf5zy-_s_yurvsFDKc"];
+
 
 var a = new Array();
-a[1] = ["Hip Hop Shop"];
-a[2] = ["Chrismukkah"];
-a[3] = ["Secret Santa"];
-a[4] = ["Feliz Navidad"];
-a[5] = ["Office Party"];
-a[6] = ["Breakin"];
-a[7] = ["EDM"];
-a[8] = ["Charleston"];
-a[9] = ["House Party"];
-a[10] = ["HipHop"];
-a[11] = ["Jingle Pop"];
-a[12] = ["Cats"];
-a[13] = ["Espanol"];
-a[14] = ["Classic"];
-a[15] = ["Honkey Tonk"];
-a[16] = ["Soul"];
-a[17] = ["80's"];
-a[18] = ["Oh Hannukkah"];
-a[19] = ["Benna 19"];
-a[20] = ["Ulli 20"];
-a[21] = ["Kempe 22"];
-a[22] = ["Michel 21"];
-a[23] = ["Charlie 23"];
-a[24] = ["pic Benna 24"];
-a[25] = ["pic Ulli 25"];
-a[26] = ["pic Kempe 26"];
-a[27] = ["pic Michel 27"];
-a[28] = ["pic Charli 28"];
+a[1] = ["1 Hip Hop Shop"];
+a[2] = ["2 Chrismukkah"];
+a[3] = ["3 Secret Santa"];
+a[4] = ["4 Feliz Navidad"];
+a[5] = ["5 Office Party"];
+a[6] = ["6Breakin"];
+a[7] = ["7 EDM"];
+a[8] = ["8 Charleston"];
+a[9] = ["9 House Party"];
+a[10] = ["10 HipHop"];
+a[11] = ["11 Jingle Pop"];
+a[12] = ["12 Cats"];
+a[13] = ["13 Espanol"];
+a[14] = ["14 Classic"];
+a[15] = ["15 Honkey Tonk"];
+a[16] = ["16 Soul"];
+a[17] = ["17 80's"];
+a[18] = ["18 Oh Hannukkah"];
+a[19] = ["19 Benna 19"];
+a[20] = ["20 Ulli 20"];
+a[21] = ["21 Kempe 22"];
+a[22] = ["22 Michel 21"];
+a[23] = ["23 Charlie 23"];
+a[24] = ["24 pic Benna 24"];
+
 
 var myVideo = document.getElementById("video1");
 var mypic = document.getElementById("pic1");
@@ -73,7 +87,7 @@ function playPause() {
 
 function changevid(n) {
   myVideo.src = v[n];
-  mypic.src = "";
+  mypic.src = p[n];
   document.getElementById("fn").innerHTML =  v[n];
   document.getElementById("na").innerHTML =  a[n];
   playPause();
@@ -88,7 +102,7 @@ function changepic(n) {
 }
 
 function settxt() {
-  for (i = 1; i <= 28; i++) {
+  for (i = 1; i <= 24; i++) {
     b = "b" + i;
     document.getElementById(b).innerHTML = a[i];};
 }
