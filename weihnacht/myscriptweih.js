de530905c476ba550cee4fc20b8d778a0cfcd45d
fambueschleb <fambueschleb@t-online.de>
2021-12-23 15:13:08 +0100

    update 23,12,

--- weihnacht/myscriptweih.js
+++ weihnacht/myscriptweih.js
@@ -1,5 +1,3 @@
-//<script>
-
 var v = new Array();
 var p = new Array();
 var prev = "https://drive.google.com/uc?export=download&id=";
@@ -10,15 +8,15 @@ v[1] = [prev + "1F3oS8usip2DCSX9tZHxjjSmEFNR3GmaW"];
 v[2] = [prev + "115OYq2mcne3TXbIdHYfY2TRwH6TRCqaY"];
 v[3] = [""];
 
-p[1] = [prevpic + "1xmTNw2vFUBEDc9dLd_pv1jWv6LqyjAqM"];
-p[2] = [prevpic + "1V9on0is0zIG-X3m4TDVxKUqjtyp-VOkc"];
-p[3] = [prevpic + "1dZ-85-Y4WzzljIfiVWV9p33rhVoyBY0t"];
-p[4] = [prevpic + "1sj4nLIFh4Nm4H-W0VAQ35W5VKagtUzr8"];
-p[5] = [prevpic + "1Tmm7JGmj6dwiOTVyy61qYA2zLpXQ0NBP"];
-p[6] = [prevpic + "1hg2xEWno0Bg_x2bUS4ZVGKrx_mDpJNCR"];
-p[7] = [prevpic + "18aT6rLGqQKL-3G5TnEDDP-V1h0sQitC1"];
-p[8] = [prevpic + "1aALP3X3HLG-hMXh4vI8OrsIYrTVYNpej"];
-p[9] = [prevpic + "1f8guZwi5yeHlUK_vuSw1fijC7xFw3f6P"];
+//p[1] = [prevpic + "1xmTNw2vFUBEDc9dLd_pv1jWv6LqyjAqM"];
+p[1] = [prevpic + "1V9on0is0zIG-X3m4TDVxKUqjtyp-VOkc"];
+p[2] = [prevpic + "1dZ-85-Y4WzzljIfiVWV9p33rhVoyBY0t"];
+p[3] = [prevpic + "1sj4nLIFh4Nm4H-W0VAQ35W5VKagtUzr8"];
+p[4] = [prevpic + "1Tmm7JGmj6dwiOTVyy61qYA2zLpXQ0NBP"];
+p[5] = [prevpic + "1hg2xEWno0Bg_x2bUS4ZVGKrx_mDpJNCR"];
+//p[7] = [prevpic + "18aT6rLGqQKL-3G5TnEDDP-V1h0sQitC1"];
+p[6] = [prevpic + "1aALP3X3HLG-hMXh4vI8OrsIYrTVYNpej"];
+p[7] = [prevpic + "1f8guZwi5yeHlUK_vuSw1fijC7xFw3f6P"];
 p[10] = [prevpic + "1uV70LnwG3B7pgPpe5Iz-y_jhVBMNrZON"];
 
 var a = new Array();
@@ -27,7 +25,7 @@ a[2] = ["Video 2"];
 a[3] = ["nächstes Bild"];
 
 let zaehler = 0;
-let zaehlermax = 10;
+let zaehlermax = 7;
 
 //  function picshow() {
 //    document.getElementById("picid").src = "ellenbogen.gif";
@@ -50,6 +48,7 @@ function changevid(n) {
   //playPause();
   document.getElementById("video1").src = v[n];
   document.getElementById("picid").src = "";
+  document.getElementById("pid").src = "";
   }
 
 if (zaehler < zaehlermax && n > 2) {
@@ -69,7 +68,8 @@ function settxt() {
     document.button.style.color = "#000000";
     document.getElementById("b2").style.backgroundcolor  = "#000000";
     document.getElementById("picid").src = "";
-}
+    document.getElementById("picelle").src = p[10];
+    }
 
 function makeBig(n) {myVideo.width = n;}
 //</script>
