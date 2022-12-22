a0f8b12e9c485df0e04c736a4651b074d55c65ed
fambueschleb <fambueschleb@t-online.de>
2022-12-22 17:43:17 +0100

    elfyourself2022 -1

--- weihnacht7/myscriptweih.js
+++ weihnacht7/myscriptweih.js
@@ -22,11 +22,11 @@ function playPause() {
 function changevid(n) {
   if (n == "1" || n == "2")
   {
-  document.getElementById("fn").innerHTML =  a[n];
+//im Balken fn wird a[n] ausgegeben  document.getElementById("fn").innerHTML =  a[n];
   document.getElementById("video1").src = v[n];
   document.getElementById("video1").style.display = "";
-  document.getElementById("picid").style.display = "none";
-  document.getElementById("picid").src = p[10];
+//  document.getElementById("picid").style.display = "none";
+//  document.getElementById("picid").src = p[10];
   }
 else {
 if (zaehler < zaehlermax && n > 2) {
@@ -52,7 +52,7 @@ function settxt() {
   for (i = 1; i <= 1; i++) {
     b = "b" + i;
     document.getElementById(b).innerHTML = a[i];};
-    document.getElementById("video1").style.display = "none";
+    //document.getElementById("video1").style.display = "none";
     document.getElementById("picid").style.display = "none";
     }
 
